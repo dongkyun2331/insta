a559a819a9372867f4dc2717f811728c64c23718
dongkyun2331 <dongkyun2331@gmail.com>
2024-01-29 01:08:17 +0900

    styled-components

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,9 +1,17 @@
 import { isLoggedInVar } from "../apollo";
+import { useState } from "react";
+import styled, { css } from "styled-components";
+
+const Container = styled.div`
+  background-color: tomato;
+`;
+
+const Title = styled.h1``;
 
 export const Login = () => {
   return (
-    <div>
-      <button onClick={() => isLoggedInVar(true)}>Log in</button>
-    </div>
+    <Container>
+      <Title>Login</Title>
+    </Container>
   );
 };
